137e95a60f2a9822de0865be885c3c826fd0c591
Sandro-Zosiashvili <157837817+Sandro-Zosiashvili@users.noreply.github.com>
2025-09-01 20:09:28 +0300

    Refactor

--- app/(auth)/album/[id]/layout.js
+++
@@ -1,8 +0,0 @@
-// app/album/[id]/layout.js
-export async function generateStaticParams() {
-    return [{id: '19'}, {id: '20'}, {id: '21'}];
-}
-
-export default function Layout({children}) {
-    return <>{children}</>;
-}
--- app/(auth)/artistlist/[id]/layout.js
+++
@@ -1,7 +0,0 @@
-export async function generateStaticParams() {
-    return [{ id: '29' }, { id: '31' }, { id: '33' },{ id: '34'}];
-}
-
-export default function Layout({ children }) {
-    return <>{children}</>;
-}
